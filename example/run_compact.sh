8fffba0f4c01e38ee35d2a8e7089df594aa38817
okunishinishi <okunishinishi@gmail.com>
2013-09-23 12:30:40 +0900

    add command

---
+++ example/run_compact.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CURRENT=$(cd "$(dirname $0)" && pwd)
+
+SRC_FILE=${CURRENT}/src/index.html
+OUT_FILE=${CURRENT}/compact.html
+tk_h=${CURRENT}/../bin/tek-html.js
+
+${tk_h} compact ${SRC_FILE} ${OUT_FILE}
---
+++ example/src/js/index.js
@@ -0,0 +1,5 @@
+(function ($) {
+    $(function () {
+        console.log('[index.js] loaded!');
+    });
+})(jQuery);

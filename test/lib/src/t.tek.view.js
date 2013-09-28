f0c34274baadfc55f9c8a2c4a009481a9143282c
okunishinishi <okunishinishi@gmail.com>
2013-09-28 14:14:08 +0900

    bug fix

--- test/lib/src/t.tek.view.js
+++ test/lib/src/t.tek.view.js
@@ -6,6 +6,6 @@ var should = require('should'),
     index = require('../../../lib/src/javascripts/tek.view.js/index');
 
 exports.toStringTest = function (test) {
-    console.log(index.toString());
+    should.exist(index.toString());
     test.done();
 };
--- test/lib/src/tek.view/t.$.js
+++ test/lib/src/tek.view/t.$.js
@@ -4,7 +4,13 @@ var should = require('should'),
 
 
 exports.FormValueTest = function (test) {
-    var formValue = new $.FormValue({k1: 1, k2: 2, i: {i1: 11, i2: 22}});
+    var formValue = new $.FormValue({
+        k1: "1", i: {i1: 11, i2: 22},
+        "namespace.name": 234
+    });
     should.exist(formValue);
+    var obj = formValue.toObj();
+    obj.k1.should.equal(1);
+    obj.namespace.should.have.property('name', 234);
     test.done();
 };

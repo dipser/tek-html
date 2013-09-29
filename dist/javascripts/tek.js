e380ed9d37a34065a79d136335c48d3633288fe5
okunishinishi <okunishinishi@gmail.com>
2013-09-29 11:43:31 +0900

    add cross browser

--- dist/javascripts/tek.js
+++ dist/javascripts/tek.js
@@ -248,6 +248,39 @@ exports.math = {};
 	};
 	
 	
+exports.window = function (window) {
+	    window.requestAnimationFrame = exports.window.dectect.requestAnimationFrame(window);
+	    window.URL = exports.window.dectect.URL(window);
+	    return window;
+	};
+	exports.window.dectect = {
+	    requestAnimationFrame: function (window) {
+	        return window.requestAnimationFrame ||
+	            window.webkitRequestAnimationFrame ||
+	            window.mozRequestAnimationFrame ||
+	            window.oRequestAnimationFrame ||
+	            window.msRequestAnimationFrame ||
+	            function (callback) {
+	                window.setTimeout(callback, 1000 / 60);
+	            };
+	    },
+	    URL: function (window) {
+	        return window.URL || window.webkitURL || window.mozURL || window.msURL;
+	    }
+	};
+	
+	exports.navigator = function (navigator) {
+	    navigator.getUserMedia = exports.navigator.detect.getUserMedia(navigator);
+	    return navigator;
+	};
+	exports.navigator.detect = {
+	    getUserMedia: function (navigator) {
+	        return navigator.getUserMedia ||
+	            navigator.webkitGetUserMedia ||
+	            navigator.mozGetUserMedia ||
+	            navigator.msGetUserMedia;
+	    }
+	};
     
     return exports;
     
---
+++ test/lib/src/tek/t.crossBrowser.js
@@ -0,0 +1,20 @@
+var crossBrowser = require('../../../../lib/src/javascripts/tek.js/09.crossBrowser'),
+    should = require('should');
+
+
+exports.windowTest = function (test) {
+    var window = {};
+    crossBrowser.window(window);
+    window.should.have.property('requestAnimationFrame');
+    test.done();
+};
+
+exports.navigatorTest = function (test) {
+    var navigator = {
+        mozGetUserMedia: function () {
+        }
+    };
+    crossBrowser.navigator(navigator);
+    navigator.should.have.property('getUserMedia');
+    test.done();
+};

c6727239f77176a6c0fb8c3dab8c3b7c185add7e
okunishinishi <okunishinishi@gmail.com>
2013-10-06 00:57:35 +0900

    bug fix

--- test/lib/src/tek.view/t.$.js
+++ test/lib/src/tek.view/t.$.js
@@ -6,11 +6,11 @@ var should = require('should'),
 exports.FormValueTest = function (test) {
     var formValue = new $.FormValue({
         k1: "1", i: {i1: 11, i2: 22},
-        "namespace.name": 234
+        "namespace.name": "234"
     });
     should.exist(formValue);
     var obj = formValue.toObj();
-    obj.k1.should.equal(1);
-    obj.namespace.should.have.property('name', 234);
+    obj.k1.should.equal("1");
+    obj.namespace.should.have.property('name', "234");
     test.done();
 };

abd4d97e0a27641ffac35a0d2e0a241729a834c5
okunishinishi <okunishinishi@gmail.com>
2013-10-17 12:57:34 +0900

    add test

---
+++ test/runnable/src/jquery.treeview/t.$.js
@@ -0,0 +1,27 @@
+require('../../../../lib/src/jquery.treeview.js.dependency.js');
+var should = require('should'),
+    $ = require('../../../../lib/src/javascripts/jquery.treeview.js/01.$.js'),
+    tv = $.treeview;
+
+exports.pTest = function (test) {
+    var p = tv.p;
+    p('item').should.equal('tv-item');
+    tv.settings.update({prefix: 'tt'});
+    p('item').should.equal('tt-item');
+    tv.settings.update({prefix: 'tv'});
+    test.done();
+};
+
+exports.ItemTest = function (test) {
+    var Item = tv['Item'];
+
+    var item = new Item().content('<span>abc</span>').children([
+        new Item('def'),
+        new Item().content('ghi')
+    ]);
+    item.toHTML()
+        .should.equal(
+            '<li class="tv-item tv-openable"><label class="tv-label"><span>abc</span></label><ul class="tv-children"><li class="tv-item"><label class="tv-label">def</label></li><li class="tv-item"><label class="tv-label">ghi</label></li></ul></li>'
+        )
+    test.done();
+};

81d85d0e74ae3f56fb851f16d6e41207bfe2f9a8
okunishinishi <okunishinishi@gmail.com>
2013-10-19 23:02:43 +0900

    add unittest

--- test/browser/javascripts/test-tek.view.js
+++ test/browser/javascripts/test-tek.view.js
@@ -16,6 +16,32 @@
             var btn = $('#section01').findByRole('submit-btn');
             strictEqual(btn.size(), 1, "findByRole");
         });
+        test("$.fn.getFormValue", function () {
+            var values = $('#section01').findByAttr('name', 'some-form').getFormValue();
+            strictEqual(values['radio01'], "radio-value01", 'radio');
+            strictEqual(values['checkbox01'][0], 'checkbox-value01', 'checkbox');
+            strictEqual(values['checkbox01'][1], 'checkbox-value03', 'checkbox');
+            strictEqual(values['radio01'], 'radio-value01', 'radio');
+            strictEqual(values['select01'], 'select-value02', 'select');
+            strictEqual(values['textarea01'], 'text-content', 'textarea');
+        });
+        test("$.fn.setFormValue", function () {
+            var form = $('#section01').findByAttr('name', 'some-form');
+            form.setFormValue({
+                "checkbox01": "checkbox-value02"
+            });
+            var values = form.getFormValue();
+            strictEqual(values['checkbox01'], 'checkbox-value02', 'checkbox');
+        });
 
+        asyncTest("$.fn.textchange", function () {
+            var form = $('#section01').findByAttr('name', 'some-form'),
+                text = form.find(':text');
+            text.textchange(function (text) {
+                equal(text, "change-text", 'text changed');
+                start();
+            }, 30);
+            text.val('change-text');
+        });
     });
 })(jQuery);

03edb8de7fdc484e3243a1737a307b7570bdfe55
okunishinishi <okunishinishi@gmail.com>
2013-11-16 11:28:27 +0900

    bugfix

--- dist/javascripts/jquery.flowchart.js
+++ dist/javascripts/jquery.flowchart.js
@@ -1,7 +1,7 @@
 /**
- * jquery.flowchart.js v0.2.32
+ * jquery.flowchart.js v0.2.33
  * - jquery plugin to create flowchart -
- * @version v0.2.32
+ * @version v0.2.33
  * @author Taka Okunishi
  * @license MIT
  * @date 2013-11-16
--- dist/javascripts/tek.js
+++ dist/javascripts/tek.js
@@ -1,7 +1,7 @@
 /**
  * tek.js
  * - javascript library for tek -
- * @version v0.2.32
+ * @version v0.2.33
  * @author Taka Okunishi
  * @license MIT
  * @date 2013-11-16
